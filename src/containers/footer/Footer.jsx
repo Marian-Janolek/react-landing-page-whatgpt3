4dfcfa1ea14803a35ed47df40f82726a0ad16149
Marian-Janolek <m.janolek@gmail.com>
2021-11-14 21:04:09 +0100

    add blog and footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
 import './footer.css';
+import gpt3Logo from '../../assets/logo.svg';
+
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <div className="gpt3-footer section-padding">
+      <div className="gpt3-footer-heading">
+        <h1 className="gradient-text">
+          Do you want to step in to the future before others{' '}
+        </h1>{' '}
+      </div>
+      <div className="gpt3-footer-btn">
+        <p>request early access</p>
+      </div>
+      <div className="gpt3-footer-links">
+        <div className="gpt3-footer-links-logo">
+          <img src={gpt3Logo} alt="logo" />
+          <p>
+            Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved
+          </p>
+        </div>
+        <div className="gpt3-footer-links-div">
+          <h4>Links</h4>
+          <p>overrons</p>
+          <p>social media</p>
+          <p>counters</p>
+          <p>contact</p>
+        </div>
+        <div className="gpt3-footer-links-div">
+          <h4>conpamy</h4>
+          <p>terms & conditions</p>
+          <p>privacy policy</p>
+          <p>contact</p>
+        </div>
+        <div className="gpt3-footer-links-div">
+          <h4>get in touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085-132567</p>
+          <p>info@payme.net</p>
+        </div>
+      </div>
+      <div className="gpt3-footer-copyright">
+        <p>@2021 GPT-3. All rights reserved.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
