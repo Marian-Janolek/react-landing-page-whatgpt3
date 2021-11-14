4f04780b1e2c93bc9af45a418476577dc1929c74
Marian-Janolek <m.janolek@gmail.com>
2021-11-14 19:49:21 +0100

    add cta

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 import './cta.css';
 
 const Cta = () => {
-  return <div>cta</div>;
+  return (
+    <div className="gpt3-cta">
+      <div className="gpt3-cta-content">
+        <p>request early access</p>
+        <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className="gpt3-cta-btn">
+        <button type="button">get started</button>
+      </div>
+    </div>
+  );
 };
 
 export default Cta;
